25b51d75a7c1bf8d718d2e5127ff2ab2afc353f6
miro <minev.miro@gmail.com>
2021-02-01 17:46:12 +0200

    Add 'About' screen

---
+++ lib/about.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:package_info/package_info.dart';
+
+class About extends StatefulWidget {
+  About();
+
+  @override
+  _AboutState createState() => _AboutState();
+}
+
+class _AboutState extends State<About> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('About'),
+        ),
+        body: _buildListView(),
+        resizeToAvoidBottomInset: false);
+  }
+
+
+  Future<String> getVersionNumber() async {
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    String appName = packageInfo.appName;
+    String packageName = packageInfo.packageName;
+    String version = packageInfo.version;
+    String buildNumber = packageInfo.buildNumber;
+
+    return version + "(" + buildNumber + ")";
+  }
+
+  ListView _buildListView() {
+    return ListView(
+      children: <Widget>[
+        // ...
+        // A title for the subsection:
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Text(
+                "About",
+                style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500),
+              ),
+            ),
+            Divider(),
+          ],
+        ),
+        // The version tile :
+        ListTile(
+          enabled: false,
+          title: Text("Version"),
+          trailing: FutureBuilder(
+            future: getVersionNumber(),
+            builder: (BuildContext context, AsyncSnapshot<String> snapshot) =>
+                Text(
+                  snapshot.hasData ? snapshot.data : "Loading ...",
+                  style: TextStyle(color: Colors.black38),
+                ),
+          ),
+        ),
+        // ...
+      ],
+    );
+  }
+}
--- lib/my_app_1.dart
+++ lib/my_app_1.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:package_info/package_info.dart';
 
+import 'about.dart';
 import 'cart.dart';
 import 'dish_object.dart';
 
@@ -27,8 +27,6 @@ class MyHomePage1 extends StatefulWidget {
 }
 
 class _MyHomePage1State extends State<MyHomePage1> {
-  String version;
-
   List<Dish> _dishes = List<Dish>();
 
   List<Dish> _cartList = List<Dish>();
@@ -43,7 +41,7 @@ class _MyHomePage1State extends State<MyHomePage1> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(version),
+        title: Text(widget.title),
         actions: <Widget>[
           Padding(
             padding: const EdgeInsets.only(right: 16.0, top: 8.0),
@@ -82,19 +80,32 @@ class _MyHomePage1State extends State<MyHomePage1> {
                   );
               },
             ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 16.0, top: 8.0),
+            child: GestureDetector(
+              child: Stack(
+                alignment: Alignment.topCenter,
+                children: <Widget>[
+                  Icon(
+                    Icons.info,
+                    size: 36.0,
+                  ),
+                ],
+              ),
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => About(),
+                  ),
+                );
+              },
+            ),
           )
         ],
       ),
       body: _buildGridView(),
     );
-  }
-
-  void getVersion() async {
-    PackageInfo packageInfo = await PackageInfo.fromPlatform();
-    String appName = packageInfo.appName;
-    String packageName = packageInfo.packageName;
-    version = packageInfo.version;
-    String buildNumber = packageInfo.buildNumber;
   }
 
 //  ListView _buildListView() {
